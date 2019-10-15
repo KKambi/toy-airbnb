89fa337cf6d017c84ce574e72b50c85f5657a6e2
KKambi <37494867+KKambi@users.noreply.github.com>
2019-10-15 17:25:09 +0900

    Add: respond token when login api request
    - 로그인 api에 리퀘스트할 때 유저정보를 검증하고 토큰을 돌려주는
    컨트롤러 구현

--- airbnb/routes/api/auth/auth.controller.js
+++ airbnb/routes/api/auth/auth.controller.js
@@ -1,17 +1,21 @@
-/*
-    POST /api/auth/register
-    {
-        username,
-        password
-    }
-*/
+const User = require('../../../src/models/User')
+const util_auth = require('../../../src/javascripts/util/util_auth')
+const util_api = require('../../../src/javascripts/util/util_api')
 
 const authController = {
     register(req, res){
         res.send('register api is working')
     },
-    login(req, res){
-        res.send('login api is working')
+
+    async login(req, res){
+        const { uid, password } = req.body
+        try{
+            const user = await User.findOne({ where: {uid: uid} })
+            const token = await util_auth.authenticate(req, user, password)
+            util_api.respond(res, "Login Success", token)
+        } catch (err) {
+            util_api.onError(res, err)
+        }
     },
 }
 
