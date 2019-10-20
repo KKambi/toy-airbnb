4879254fea2cb688960bd21c3cb65219d142b890
KKambi <37494867+KKambi@users.noreply.github.com>
2019-10-20 10:56:15 +0900

    Add: react fundamental practice
    - react 실습을 위한 기초 파일

--- airbnb-react/src/App.js
+++ airbnb-react/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const a = () => {
+  return 2;
+}
+
+function FilterBar({ favorite }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Filter Bar</h1>
+      <h2>{favorite}</h2>
     </div>
   );
 }
 
-export default App;
+function App() {
+  return (
+    <div>
+      hello!
+      <FilterBar favorite="kimchi" arr={[1, 2, 3]} foo />
+    </div>
+  );
+}
+
+export default A;
---
+++ airbnb-react/src/FilterBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FilterBar(){
+    return (
+        <h3>Filter Bar!</h3>
+    )
+}
+
+export default FilterBar;
