e28199b7c2ce6d374bea2c70699143a77e53f077
KKambi <37494867+KKambi@users.noreply.github.com>
2019-10-22 20:06:22 +0900

    Update: findAll query to findAllAndCount in stays model
    - 숙소 개수를 알기 위해 sequezlie 메소드 변경

--- airbnb/server/routes/api/reservations/reservations.controller.js
+++ airbnb/server/routes/api/reservations/reservations.controller.js
@@ -1,38 +1,41 @@
-const util_api = require('../../../javascripts/util/util_api')
-const models = require('../../../database/models')
+const util_api = require('../../../javascripts/util/util_api');
+const models = require('../../../database/models');
 
 const reservationsController = {
-    // 새로운 예약을 입력하는 api
-    // TODO: 예약이 겹치지 않도록 검사하는 validation 필요함
-    async create(req, res){
-        const guest_id = req.decoded.id
-        const { stay_id, check_in, check_out } = req.body
-        try{
-            await models.Reservation.create({
-                stay_id, guest_id, check_in, check_out
-            })
-        } catch(err) {
-            util_api.respondError(res, err)
-        }
-        util_api.respondSucess(res, "Reservation suceess")
-    },
-
-    // 예약을 취소하는 api
-    async delete(req, res){
-        const { id } = req.body
-        const user_id = req.decoded.id
-        try{
-            await models.Reservation.destroy({
-                where: { 
-                    id,
-                    guest_id: user_id
-                }
-            })
-        } catch(err) {
-            util_api.respondError(res, err)
-        }
-        util_api.respondSucess(res, "Cancel reservation suceess")
+  // 새로운 예약을 입력하는 api
+  // TODO: 예약이 겹치지 않도록 검사하는 validation 필요함
+  async create(req, res) {
+    const guest_id = req.decoded.id;
+    const { stay_id, check_in, check_out } = req.body;
+    try {
+      await models.Reservation.create({
+        stay_id,
+        guest_id,
+        check_in,
+        check_out,
+      });
+    } catch (err) {
+      util_api.respondError(res, err);
     }
-}
+    util_api.respondSucess(res, 'Reservation suceess');
+  },
 
-module.exports = reservationsController
+  // 예약을 취소하는 api
+  async delete(req, res) {
+    const { id } = req.body;
+    const user_id = req.decoded.id;
+    try {
+      await models.Reservation.destroy({
+        where: {
+          id,
+          guest_id: user_id,
+        },
+      });
+    } catch (err) {
+      util_api.respondError(res, err);
+    }
+    util_api.respondSucess(res, 'Cancel reservation suceess');
+  },
+};
+
+module.exports = reservationsController;
--- airbnb/server/routes/api/stays/stays.controller.js
+++ airbnb/server/routes/api/stays/stays.controller.js
@@ -2,10 +2,10 @@ const util_api = require('../../../javascripts/util/util_api');
 const models = require('../../../database/models');
 
 const stayController = {
-  // 모든 유저 정보를 반환하는 api
+  // 모든 숙소 정보와 카운트를 반환하는 api
   async getStays(req, res) {
     try {
-      const stays = await models.Stay.findAll({
+      const stays = await models.Stay.findAndCountAll({
         attributes: ['id', 'name', 'price', 'guest', 'type', 'image', 'beds', 'bedrooms', 'bathrooms', 'host_id', 'createdAt', 'updatedAt'],
         offset: 1 * 10, //FIXME: page변수로 현재 페이지에 따른거 보여줘야함
         limit: 10,
@@ -16,7 +16,7 @@ const stayController = {
     }
   },
 
-  // 특정 유저 정보를 반환하는 api
+  // 특정 숙소 정보를 반환하는 api
   async getStayById(req, res) {
     const id = req.query.id || req.body.id || req.params.id;
     try {
@@ -29,6 +29,17 @@ const stayController = {
       util_api.respondError(res, err);
     }
   },
+
+  // 총 숙소 개수를 반환하는 api
+  async getStaysCount(req, res) {
+    try {
+      const stays = await models.Stay.count({
+      });
+      util_api.respondData(res, stays);
+    } catch (err) {
+      util_api.respondError(res, err);
+    }
+  }
 };
 
 module.exports = stayController;
