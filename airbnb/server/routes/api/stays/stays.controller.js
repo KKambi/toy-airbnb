e16746bf5e270a8feb0045a2507fa87fbfbb8f10
KKambi <37494867+KKambi@users.noreply.github.com>
2019-10-22 15:46:23 +0900

    Update: findAll options in stay model
    - stay api에서 쿼리성능향상을 위해 findAll에 칼럼을 지정해줌
    - 현재 offset과 limit을 지정해놓은 상태,, 나중에 페이지네이션을 구현하기
    위함

--- airbnb/server/routes/api/stays/stays.controller.js
+++ airbnb/server/routes/api/stays/stays.controller.js
@@ -1,30 +1,34 @@
-const util_api = require('../../../javascripts/util/util_api')
-const models = require('../../../database/models')
+const util_api = require('../../../javascripts/util/util_api');
+const models = require('../../../database/models');
 
 const stayController = {
-    // 모든 유저 정보를 반환하는 api
-    async getStays(req, res) {
-        try{
-            const stays = await models.Stay.findAll()
-            models.Stay.create()
-            util_api.respondData(res, stays)
-        } catch(err) {
-            util_api.respondError(res, err)
-        }
-    },
-
-    // 특정 유저 정보를 반환하는 api
-    async getStayById(req, res){
-        const id = req.query.id || req.body.id
-        try{
-            const stay = await models.Stay.findOne({ 
-                where: { id }
-            })
-            util_api.respondData(res, stay)
-        } catch(err) {
-            util_api.respondError(res, err)
-        }
+  // 모든 유저 정보를 반환하는 api
+  async getStays(req, res) {
+    try {
+      const stays = await models.Stay.findAll({
+        attributes: ['id', 'name', 'price', 'guest', 'type', 'image', 'beds', 'bedrooms', 'bathrooms', 'host_id', 'createdAt', 'updatedAt'],
+        offset: 1 * 50, //FIXME: page변수로 현재 페이지에 따른거 보여줘야함
+        limit: 50,
+      });
+      util_api.respondData(res, stays);
+    } catch (err) {
+      util_api.respondError(res, err);
     }
-}
+  },
 
-module.exports = stayController
+  // 특정 유저 정보를 반환하는 api
+  async getStayById(req, res) {
+    const id = req.query.id || req.body.id || req.params.id;
+    try {
+      const stay = await models.Stay.findOne({
+        where: { id },
+        attributes: ['id', 'name', 'price', 'guest', 'type', 'image', 'beds', 'bedrooms', 'bathrooms', 'host_id', 'createdAt', 'updatedAt'],
+      });
+      util_api.respondData(res, stay);
+    } catch (err) {
+      util_api.respondError(res, err);
+    }
+  },
+};
+
+module.exports = stayController;
