ac38a321049992d776987e8f600fa4dc7b595469
KKambi <37494867+KKambi@users.noreply.github.com>
2019-10-22 14:14:34 +0900

    Add: eslint rule for CSRF in window
    - 윈도우에서 CSRF로 개행이 되는 점을 지적하는 eslint rule을 무시하도록
    설정
    - 윈도우에선 어쩔 수 없음ㅠㅠ 깃 설정을 향후 바꿔줘야 함.

--- airbnb/.eslintrc.js
+++ airbnb/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         "commonjs": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": ["eslint:recommended", "airbnb"],
     "globals": {
         "process": true,
         "Atomics": "readonly",
