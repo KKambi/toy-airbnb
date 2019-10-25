f48bf0b9048b2790dc0dc884785971fa603e422b
KKambi <37494867+KKambi@users.noreply.github.com>
2019-10-25 09:55:49 +0900

    Refactor: integrate server file and module
    - 서버 관련 파일들을 server directory에 통합

---
+++ airbnb/server/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": ["eslint:recommended", "airbnb"],
+    "globals": {
+        "process": true,
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "sourceType": "module"
+    },
+    "rules": {
+      "linebreak-style": 0
+    }
+};
